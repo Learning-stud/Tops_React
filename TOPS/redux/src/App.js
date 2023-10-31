9ab1070209c49a30aa13d0e574aad58f360df21a
Learning-stud <jayahalpara123456@gmail.com>
2023-10-31 14:53:46 +0530

    ...

--- TOPS/redux/src/App.js
+++ TOPS/redux/src/App.js
@@ -5,13 +5,15 @@ import Dashboard from "./features/website/pages/Dashboard";
 import Manage_data from "./features/website/pages/Manage_data";
 import Add_data from "./features/website/pages/Add_data";
 import Contact from "./features/website/pages/Contact";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 function App() {
   return (
     <BrowserRouter>
-     
+     <ToastContainer></ToastContainer>
       <Routes>
           <Route path="/" index element={<><Header/><Dashboard/><Footer/></>}></Route>
           <Route path="/add_data"  element={<><Header/><Add_data/><Footer/></>}></Route>
