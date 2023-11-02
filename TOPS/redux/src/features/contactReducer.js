90f3daea4b187d302c3ad7eea4c3e3171b8372c4
Learning-stud <jayahalpara123456@gmail.com>
2023-11-02 13:49:16 +0530

    create contect reducer componenet and manage data

--- TOPS/redux/src/features/contactReducer.js
+++ TOPS/redux/src/features/contactReducer.js
@@ -1,3 +1,8 @@
+import axios from "axios";
+
+const ALLCONTACT="ALLCONTACT";
+const SINGLECONTACT="SINGLECONTACT";
+
 const objectinia={
     allcontact:[],
     singlecontact:{}
@@ -5,7 +10,51 @@ const objectinia={
 
 const contactReducer=(state=objectinia,action)=>{
 
+    switch(action.type)
+    {
+        case "ALLCONTACT":
+        return{
+            ...state,
+            allcontact:action.payload
+        }
+        case "SINGLECONTACT":
+        return{
+            ...state,
+            singlecontact:action.payload
+        }
+        
+    }
     return state;
 }
 
 export default contactReducer;
+
+
+
+const getcontact=(data)=>({
+    type:ALLCONTACT,
+    payload:data
+})
+
+export const fetchcontact=()=>{
+    return function(dispatch){
+        axios.get('http://localhost:3000/contact')
+        .then((resp)=>{
+            dispatch(getcontact(resp.data))
+        })
+    }
+}
+
+const getsinglecontact=(data)=>({
+    type:SINGLECONTACT,
+    payload:data
+})
+
+export const fetchsinglecontact=(id)=>{
+    return function(dispatch){
+        axios.get(`http://localhost:3000/contact/${id}`)
+        .then((resp)=>{
+            dispatch(getsinglecontact(resp.data))
+        })
+    }
+}
