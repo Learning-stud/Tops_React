c9e399978d1dfd673a1a95e3ff43848f5d724806
Learning-stud <jayahalpara123456@gmail.com>
2023-11-02 13:48:35 +0530

    Changing the routing location

--- TOPS/redux/src/App.js
+++ TOPS/redux/src/App.js
@@ -5,20 +5,20 @@ import Dashboard from "./features/website/pages/Dashboard";
 import Manage_data from "./features/website/pages/Manage_data";
 import Add_data from "./features/website/pages/Add_data";
 import Contact from "./features/website/pages/Contact";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Manage_contact from "./features/website/pages/Manage_contact";
 
 
 
 function App() {
   return (
     <BrowserRouter>
-     <ToastContainer></ToastContainer>
+     
       <Routes>
           <Route path="/" index element={<><Header/><Dashboard/><Footer/></>}></Route>
           <Route path="/add_data"  element={<><Header/><Add_data/><Footer/></>}></Route>
           <Route path="/manage_data"  element={<><Header/><Manage_data/><Footer/></>}></Route>
           <Route path="/contact"  element={<><Header/><Contact/><Footer/></>}></Route>
+          <Route path="/manage_contact"  element={<><Header/><Manage_contact/><Footer/></>}></Route>
       </Routes>
  </BrowserRouter>
   );
