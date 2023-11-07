b8098900d3e22c240c9f23d81d0895fbbd096ae2
Learning-stud <jayahalpara123456@gmail.com>
2023-11-07 17:46:28 +0530

    ....

--- Practice/counter-project/src/App.jsx
+++ Practice/counter-project/src/App.jsx
@@ -8,6 +8,11 @@ function App() {
     console.log("clicked", Math.random);
     // counter = counter + 1;
     setCounter(counter + 1);
+    /* downside it will create an batch and repeat the same work  */
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
   };
   /*  for SubStracTion  */
   const minusValue = () => {
@@ -15,7 +20,7 @@ function App() {
   };
   return (
     <>
-      <h1> COUNTER  </h1>
+      <h1> COUNTER </h1>
       <h1>uounter value : {counter}</h1>
       <button onClick={addValue}> Add Value </button>
       <br />
