c394e58ae413ec58294fb951999d234b504175f8
Learning-stud <jayahalpara123456@gmail.com>
2023-11-07 19:09:16 +0530

    Background color changing using state......

---
+++ Practice/background-changer/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+  const [message, setMessage] = useState("");
+
+  const changeColor = (newColor) => {
+    setColor(newColor);
+    setMessage("Color Changed to " + newColor);
+  };
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="justify-center align-middle flex pt-5 text-2xl bottom-10 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-3 rounded-md">
+          <button
+            onClick={() => changeColor("red")}
+            className="outline-none px-4 py-1 text-white shadow-xl bg-red-600 rounded-md"
+          >
+            {" "}
+            Red{" "}
+          </button>
+          <button
+            onClick={() => changeColor("green")}
+            className="outline-none px-4 py-1 text-white shadow-xl bg-green-600 rounded-md"
+          >
+            {" "}
+            Green{" "}
+          </button>
+          <button
+            onClick={() => changeColor("blue")}
+            className="outline-none px-4 py-1 text-white shadow-xl bg-blue-500 rounded-md"
+          >
+            {" "}
+            Blue{" "}
+          </button>
+        </div>
+      </div>
+      <div className="  text-3xl text-slate-50  text-center justify-center pt-96 ">{message}</div>
+    </div>
+  );
+}
+
+export default App;
